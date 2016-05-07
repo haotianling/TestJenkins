f81ddc7f7ed2e822e84cc1d01e253d1a81c2d146
haotian <1364421081@qq.com>
2016-05-07 10:04:01 +0800

    新建项目

---
+++ TestJenkins/TestJenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestJenkins
+//
+//  Created by haotian on 16/5/7.
+//  Copyright © 2016年 haotian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
